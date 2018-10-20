b758f1097106b8c67ad1473c6fd5663ea383dc74
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2018-10-20 10:47:24 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const express = require('express'),
+      payment = require('./payment');
+
+
+const app = express();
+
+
+// insert view-engine ejs
+
+//app.use(require('payment.js'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', function (req, res) {
+    payment.run();
+
+    res.send("WORKS");
+});
+
+
+app.listen(3000, function () {
+    console.log("Listening on port 3000");
+});
